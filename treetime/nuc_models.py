8937a409d86bef78e6675c5975679e3eadc0c162
mneumann95 <88090972+mneumann95@users.noreply.github.com>
2025-01-30 01:44:42 +0100

    Update nuc_models.py

--- treetime/nuc_models.py
+++ treetime/nuc_models.py
@@ -138,8 +138,8 @@ def HKY85(mu=1.0, pi=None, kappa=0.1, **kwargs):
     if pi is None:
         pi=0.25*np.ones(4, dtype=float)
     num_chars = len(alphabets['nuc_nogap'])
-
-    if num_chars != len(pi) :
+    pi=np.array(pi)
+    if num_chars != pi.shape[0] :
         pi = np.ones((num_chars, ), dtype=float)
         print ("GTR: Warning!The number of the characters in the alphabet does not match the "
             "shape of the vector of equilibrium frequencies Pi -- assuming equal frequencies for all states.")
